3697d129d4a587a90d54a7e4faffae02fea32128
Ian Johnson <ian.johnson@canonical.com>
2018-08-09 15:32:22 -0500

    snap/snapcraft.yaml: add network interface for kong
    Obviously at some point a microservice gateway will need to make an outgoing network connection as well as an incoming one with network-bind.

--- bin/kong-wrapper.sh
+++ bin/kong-wrapper.sh
@@ -31,6 +31,7 @@ export LUA_VERSION=5.1
 export LUA_PATH="$SNAP/lualib/?.lua;$SNAP/lualib/?/init.lua;$SNAP/usr/share/lua/$LUA_VERSION/?.lua;$SNAP/usr/share/lua/$LUA_VERSION/?/init.lua;$SNAP/lib/lua/$LUA_VERSION/?.lua;$SNAP/lib/lua/$LUA_VERSION/?/init.lua;$SNAP/share/lua/$LUA_VERSION/?.lua;$SNAP/share/lua/$LUA_VERSION/?/init.lua;;"
 export LUA_CPATH="$SNAP/lualib/?.so;$SNAP/lib/lua/$LUA_VERSION/?.so;$SNAP/lib/$archLibName/lua/$LUA_VERSION/?.so;;"
 
+# vars that make perl warnings go away
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
