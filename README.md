e48bc92d2e7cf517c221d9449744a5e71c2dee14
Ian Johnson <ian.johnson@canonical.com>
2019-10-17 06:59:20 -0500

    many: do postgres setup in post-refresh hook too
    We also need to do the postgres setup in the post-refresh hook (in addition to
    the install hook) so that users who are upgraded from the current version which
    only uses Cassandra can seamlessly switch to PostgreSQL if they so choose.
    Otherwise, they would have to copy config files and manually process them, etc.
    which is a poor user experience.
    
    Add a note about this to the README.md, including how to migrate from Cassandra
    to PostgreSQL if a user desires.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- README.md
+++ README.md
@@ -6,10 +6,20 @@
 
 This repo contains necessary wrappers and config files to package and publish Kong as a snap.
 
-Note that the snap bundles Cassandra as a database provider for Kong, as Postgres doesn't work well inside a snap. Cassandra automatically runs in the background as a service in the snap. You can disable cassandra with:
+The snap currently bundles both Cassandra and PostgeSQL as possible databases to use with Kong. If you installed the snap before PostgeSQL was added to the snap, you will have Cassandra as your default, however if you install a fresh version, then you will have PostgreSQL as your database. 
+
+Since previous versions of this snap only bundled Cassandra, to not break anyone who might have had the snap previously installed running with Cassandra, the `kong-daemon` service in the snap will continue to use Cassandra if you upgrade to the new version of the snap with PostgreSQL in it. 
+
+To switch from using Cassandra to PostgreSQL is simple however if you desire. Run the following:
 
 ```bash
-$ sudo snap stop --disable kong.cassandra
+sudo snap run --shell kong -c "sed -i \"s@database = cassandra@database = postgres@\" \$SNAP_DATA/config/kong.conf"
+snap stop --disable kong.cassandra
+snap restart kong.kong-daemon
 ```
+
+And you should be using PostgreSQL now instead of Cassandra and Cassandra should not automatically startup anymore.
+
+**Note**: that this will only change the config file to tell Kong to use PostgreSQL, if you had important data for Kong stored in Cassandra, you will need to manually migrate that data as well. Consult the Kong docs for more information on how to do this.
 
 This code is released under the same license as the kong project: https://github.com/Kong/kong
---
+++ bin/common-postgres-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+# get the values of $SNAP_DATA and $SNAP using the current symlink instead of
+# the default behavior which has the revision hard-coded, which breaks after
+# a refresh
+SNAP_DATA_CURRENT=${SNAP_DATA/%$SNAP_REVISION/current}
+SNAP_CURRENT=${SNAP/%$SNAP_REVISION/current}
+
+# setup postgres db config file with env vars replaced
+if [ ! -f "$SNAP_DATA/etc/postgresql/10/main/postgresql.conf" ]; then
+    mkdir -p "$SNAP_DATA/etc/postgresql/10/main"
+    cp "$SNAP/etc/postgresql/10/main/postgresql.conf" "$SNAP_DATA/etc/postgresql/10/main/postgresql.conf"
+    # do replacement of the $SNAP, $SNAP_DATA, $SNAP_COMMON environment variables in the config files
+    sed -i -e "s@\$SNAP_COMMON@$SNAP_COMMON@g" \
+        -e "s@\$SNAP_DATA@$SNAP_DATA_CURRENT@g" \
+        -e "s@\$SNAP@$SNAP_CURRENT@g" \
+        "$SNAP_DATA/etc/postgresql/10/main/postgresql.conf"
+fi
+
+# ensure the postgres data directory exists and is owned by snap_daemon
+mkdir -p "$SNAP_DATA/postgresql" 
+chown -R snap_daemon:snap_daemon "$SNAP_DATA/postgresql" 
+
+# setup the postgres data directory
+gosu snap_daemon "$SNAP/usr/lib/postgresql/10/bin/initdb" -D "$SNAP_DATA/postgresql/10/main"
+
+# ensure the sockets dir exists and is properly owned
+mkdir -p "$SNAP_COMMON/sockets"
+chown -R snap_daemon:snap_daemon "$SNAP_COMMON/sockets" 
+
+# start postgres up and wait a bit for it
+snapctl start --enable "$SNAP_NAME.postgres"
+sleep 2
+
+# add a kong user and database in postgres - note we have to run these through
+# the psql-wrapper scripts to setup env vars properly
+gosu snap_daemon "$SNAP/bin/psql-wrapper.sh" "$SNAP/usr/bin/createuser" kong
+gosu snap_daemon "$SNAP/bin/psql-wrapper.sh" "$SNAP/usr/bin/createdb" kong
+
