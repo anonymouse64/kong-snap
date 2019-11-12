608ea5112781387b73fc562d4feabce40e3998fe
Ian Johnson <ian.johnson@canonical.com>
2019-11-12 15:58:46 -0600

    kong-daemon.sh: add disabled migrations commands
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- bin/kong-daemon.sh
+++ bin/kong-daemon.sh
@@ -9,5 +9,15 @@ until kong migrations bootstrap --conf "$KONG_CONF"; do
     sleep 5
 done
 
+# # handle additional kong migrations from previous kong versions
+# set +e
+# kong migrations list --conf "$KONG_CONF"
+# code=$?
+# set -e
+# if [ $code -eq 5 ]; then
+#   kong migrations up --conf "$KONG_CONF"
+#   kong migrations finish --conf "$KONG_CONF"
+# fi
+
 # now start kong normally
 kong start --conf "$KONG_CONF" --v
